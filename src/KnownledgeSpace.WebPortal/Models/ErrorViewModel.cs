6fdecb26bac822b964e3c8b39b5c8ee43a59c921
BuiTanLan <tanlanhcmus@gmail.com>
2020-07-15 23:51:04 +0700

    inittlaize source code structure

---
+++ src/KnownledgeSpace.WebPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KnownledgeSpace.WebPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
