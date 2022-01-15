3e0e855eb28bee476cdc803f38e74e2ab2688bd7
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-01-15 19:04:57 +0700

    update to Duende.IdentityServer

--- src/KnowledgeSpace.BackendServer/IdentityServer/IdentityProfileService.cs
+++ src/KnowledgeSpace.BackendServer/IdentityServer/IdentityProfileService.cs
@@ -1,8 +1,8 @@
 ﻿using System.Security.Claims;
 using System.Text.Json;
-using IdentityServer4.Extensions;
-using IdentityServer4.Models;
-using IdentityServer4.Services;
+using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
 using KnowledgeSpace.BackendServer.Constants;
 using KnowledgeSpace.BackendServer.Data;
 using KnowledgeSpace.BackendServer.Data.Entities;
