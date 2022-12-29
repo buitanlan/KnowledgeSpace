6790a10f8d6b73e82179a2ab11b217ce55a1229e
Bùi Tấn Lân <tanlanhcmus@gmail.com>
2022-12-30 02:03:56 +0700

    chore(): format code

--- admin-app/src/app/shared/interceptors/errors.interceptor.ts
+++ admin-app/src/app/shared/interceptors/errors.interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable, retry, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
