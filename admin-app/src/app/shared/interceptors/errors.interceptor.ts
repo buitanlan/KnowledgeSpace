7406985cafed78c400dfa8b41494a37bb560508c
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2023-01-03 17:30:14 +0700

    feat(): update interceptor

--- admin-app/src/app/shared/interceptors/errors.interceptor.ts
+++ admin-app/src/app/shared/interceptors/errors.interceptor.ts
@@ -1,41 +1,34 @@
-import { Injectable } from '@angular/core';
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable, retry, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, retry, throwError } from 'rxjs';
 
-@Injectable()
-export class ErrorsInterceptor implements HttpInterceptor {
-  constructor() {}
+export const errorInterceptor: HttpInterceptorFn = (request, next) => {
+  return next(request).pipe(retry(1), catchError(handleErrors));
+};
+const handleErrors = (error: HttpErrorResponse) => {
+  let errorMessage = '';
+  if (error.error instanceof ErrorEvent) {
+    // client-side error
+    errorMessage = `Error: ${error.error.message}`;
+  } else {
+    const applicationError = error.headers.get('Application-Error');
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(retry(1), catchError(this.handleErrors));
-  }
-  handleErrors(error: HttpErrorResponse) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      // client-side error
-      errorMessage = `Error: ${error.error.message}`;
-    } else {
-      const applicationError = error.headers.get('Application-Error');
-
-      // either application-error in header or model error in body
-      if (applicationError) {
-        return throwError(() => applicationError);
-      }
-
-      let modelStateErrors = '';
-
-      // for now just concatenate the error descriptions, alternative we could simply pass the entire error response upstream
-      for (const key in error.error) {
-        if (error.error[key]) {
-          modelStateErrors += error.error[key].description + '\n';
-        }
-      }
-
-      modelStateErrors = modelStateErrors === '' ? 'Server error' : modelStateErrors;
-      return throwError(() => modelStateErrors);
+    // either application-error in header or model error in body
+    if (applicationError) {
+      return throwError(() => applicationError);
     }
-    console.log('error', errorMessage);
-    return throwError(() => errorMessage);
+
+    let modelStateErrors = '';
+
+    // for now just concatenate the error descriptions, alternative we could simply pass the entire error response upstream
+    for (const key in error.error) {
+      if (error.error[key]) {
+        modelStateErrors += error.error[key].description + '\n';
+      }
+    }
+
+    modelStateErrors = modelStateErrors === '' ? 'Server error' : modelStateErrors;
+    return throwError(() => modelStateErrors);
   }
-}
+  console.log('error', errorMessage);
+  return throwError(() => errorMessage);
+};
